c8e15b25582fa5512665703375916bb2cf5ea453
Lucca <lucca.christiansen@mahjonglogic.com>
2024-10-21 12:50:51 +0200

    uadd_ok

--- test/part_one_test.c
+++ test/part_one_test.c
@@ -1,6 +1,7 @@
 #include "unity.h"
 #include "unity_internals.h"
 #include <stdbool.h>
+#include <stdio.h>
 void test_pp_2_12_bitwise_expressions(void) {
   size_t x = 0x87654321;
   TEST_ASSERT_EQUAL(0x21, 0xFF & x);
@@ -33,4 +34,22 @@ void test_shift_operations(void) {
   TEST_ASSERT_EQUAL(shift2(0x000000C9u), (int)0xFFFFFFC9);
   TEST_ASSERT_EQUAL(shift1(0xedcba987u), (int)0x00000087);
   TEST_ASSERT_EQUAL(shift2(0xedcba987u), (int)0xFFFFFF87);
+}
+
+int uadd_ok(unsigned x, unsigned y) {
+  unsigned carry = 0;
+  for (unsigned i = 0; i < 32; ++i) {
+    unsigned x_digit = (x & (1 << i)) >> i;
+    unsigned y_digit = (y & (1 << i)) >> i;
+    carry = (x_digit + y_digit + carry) >= 2;
+  }
+  return !carry;
+}
+
+void test_uadd_ok(void) {
+  TEST_ASSERT_EQUAL(1, uadd_ok(1, 1));
+  TEST_ASSERT_EQUAL(0, uadd_ok(0x80000000, 0x80000000));
+  TEST_ASSERT_EQUAL(0, uadd_ok(0xFFFFFFFF, 0xFFFFFFFF));
+  TEST_ASSERT_EQUAL(1, uadd_ok(0xF0000000, 0x0FFFFFFF));
+  TEST_ASSERT_EQUAL(0, uadd_ok(0x7FFFFFFF, 0x80000001));
 }
